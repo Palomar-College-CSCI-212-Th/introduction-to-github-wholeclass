93039f137c0e18bbe113c398b83e158a0fe3ce2f
Lemuel Davis <ldavis2@palomar.edu>
2016-10-27 14:37:55 -0700

    Changes to print John Doe.

--- intro_to_github/main.c
+++ intro_to_github/main.c
@@ -1,9 +1,19 @@
 // Module Name :	    main.c
-// Description :		Calls assembly functions
+// Description :		Calls assembly functions that prints names
 
 #include <stdio.h>
 
+// Prototypes
+void PrintJohnDoe();
+
+// Main
 int main(void)
 {
+	PrintJohnDoe();
+
+	// Make a new function to print your name.
+	// Create a new macro in your function as in the example code.
+	// Your name should be printed three times.
+
 	return 0;
 }
