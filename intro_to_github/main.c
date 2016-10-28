f35881c8ae6e0fe935fdb67e7a8b8b3041b8f044
justin van bibber <vanbi002@cougars.csusm.edu>
2016-10-27 19:01:38 -0700

    justin van bibber, checking in!

--- intro_to_github/main.c
+++ intro_to_github/main.c
@@ -6,13 +6,14 @@
 // Prototypes
 void PrintJohnDoe();
 void PrintLemDavis();
+void PrintJustinVanBibber();
 
 // Main
 int main(void)
 {
 	PrintJohnDoe();
 	PrintLemDavis();
-
+	PrintJustinVanBibber();
 	// Make a new function to print your name.
 	// Create a new macro in your function as in the example code.
 	// Your name should be printed three times.
