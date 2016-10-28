c821a0d56278844264626687e9554da7d02df6b2
wwells4 <william.wells@gmx.com>
2016-10-27 19:17:05 -0700

    New Stuff

--- intro_to_github/main.c
+++ intro_to_github/main.c
@@ -6,12 +6,14 @@
 // Prototypes
 void PrintJohnDoe();
 void PrintLemDavis();
+void PrintWilliamWells();
 
 // Main
 int main(void)
 {
 	PrintJohnDoe();
 	PrintLemDavis();
+	PrintWilliamWells();
 
 	// Make a new function to print your name.
 	// Create a new macro in your function as in the example code.
